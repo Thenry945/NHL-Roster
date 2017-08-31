cd16a33a071895de145dbabe8f01b43d12cd784d
Thenry945 <travis@henrichs.cc>
2017-08-31 13:08:19 -0500

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.set('port',port);
 app.use(express.static(__dirname+'/public'));
